aaca3416cf68a1c0566edbdc3c75e3f1df0f8b56
Мельник Дмитрий Сергеевич <34408853+Mel05@users.noreply.github.com>
2025-04-09 11:19:50 +1000

    Update getRequestPath.ts

--- src/utils/getRequestPath.ts
+++ src/utils/getRequestPath.ts
@@ -1,7 +1,8 @@
 export const getRequestPath = () => {
-	const requestPath = `https://todos-server.up.railway.app/`
-	// const requestPath = `https://todos-server-supermadmel.onrender.com`
+	// const requestPath = `https://todos-server.up.railway.app/`
+	const requestPath = `https://todos-server-supermadmel.onrender.com`
 	// const requestPath = `http://localhost:3011`
+	// const requestPath = `http://localhost:8080`
 
 	return {
 		requestPath,
