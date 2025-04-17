a6196783660bfc902913bbae409fc246ad446054
Мельник Дмитрий Сергеевич <34408853+Mel05@users.noreply.github.com>
2025-04-17 14:55:33 +1000

    Update Wrapper.tsx

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -10,7 +10,7 @@ import Footer from './Footer'
 
 const Wrapper = () => {
 	return (
-		<div className='-wrapper w-screen min-h-screen grid grid-rows-[auto_1fr_auto]  bg-orange-400 text-zinc-900 dark:bg-zinc-900 dark:text-pink-400 overflow-hidden landscape:h-auto'>
+		<div className='-wrapper w-screen h-screen grid grid-rows-[auto_1fr_auto]  bg-orange-400 text-zinc-900 dark:bg-zinc-900 dark:text-pink-400 overflow-hidden landscape:h-auto'>
 			<Bigpic className='absolute left-0 top-0 w-full h-full object-cover fill-zinc-900 dark:fill-pink-400 z-0' />
 			<Navbar />
 			<Routes>
