f59e4d2dfff94f154bc8064168f1adcd9af13d09
Мельник Дмитрий Сергеевич <34408853+Mel05@users.noreply.github.com>
2025-05-07 22:33:44 +1000

    Update Wrapper.tsx

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -8,16 +8,19 @@ import Main from '../pages/Main'
 import Pagination from './common/Pagination'
 import Footer from './Footer'
 
+/// Костыль для Onrender-а
+import Onrender from '../pages/Onrender'
+
 const Wrapper = () => {
 	return (
-		<div className='-wrapper w-screen h-screen grid grid-rows-[auto_1fr_auto]  bg-orange-400 text-zinc-900 dark:bg-zinc-900 dark:text-pink-400 overflow-hidden landscape:h-auto md:landscape:h-screen'>
-			<Bigpic className='absolute left-0 top-0 w-full h-full object-cover fill-zinc-900 dark:fill-pink-400 landscape:h-auto landscape:w-auto  md:landscape:w-full md:landscape:h-full z-0' />
+		<div className='-wrapper relative w-screen h-screen grid grid-rows-[auto_1fr_auto]  bg-orange-400 text-zinc-900 dark:bg-zinc-900 dark:text-pink-400 overflow-hidden landscape:h-auto md:landscape:h-screen'>
+			<Bigpic className='absolute left-0 top-0 w-full h-full object-cover fill-zinc-900 dark:fill-pink-400    z-0' />
 			<Navbar />
 			<Routes>
 				<Route path='/' element={<Login />} />
 				<Route path='/:userNickName' element={<Main />} />
+				<Route path='/onrender' element={<Onrender />} />
 			</Routes>
-
 			<Pagination />
 			<Footer />
 		</div>
